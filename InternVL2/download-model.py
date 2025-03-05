183850692989db0955ed4036d1f21bdf010ec755
Zeeshan Hyder <109053785+Zeeshann1@users.noreply.github.com>
2025-03-05 13:50:54 +0800

    InternVL2

---
+++ InternVL2/download-model.py
@@ -0,0 +1,6 @@
+from huggingface_hub import snapshot_download
+
+model_dir = snapshot_download(repo_id ="OpenGVLab/InternVL2-8B", local_dir = "D:\InternVL")
+                              
+
+                              
